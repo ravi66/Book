ec3dfaa8c92e1c773e7b74c51ce8ccc786c20a26
Ravi Kamat <ravitkamat@hotmail.com>
2024-02-06 21:31:19 +0000

    TransList credit colour

--- Book.App/Models/Constants.cs
+++ Book.App/Models/Constants.cs
@@ -3,7 +3,7 @@
     public static class Constants
     {
         public const string BookVersion = "v1.0.00";
-        public const string PositiveValueCssClass = "mud-body-text ";   // primary-text ";
+        public const string PositiveValueCssClass = "mud-primary-text ";
         public const string NegativeValueCssClass = "mud-secondary-text ";
     }
 }
