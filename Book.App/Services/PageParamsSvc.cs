9e648ae213200b22fc38ddfde6169ad6526a029f
Ravi Kamat <ravitkamat@hotmail.com>
2024-08-19 12:23:26 +0100

    MainLayout. Add Mini Drawer Nav Menu. BookAppBar. Remove Settings and Back tooltip states destination page.

--- Book.App/Services/PageParamsSvc.cs
+++ Book.App/Services/PageParamsSvc.cs
@@ -1,4 +1,6 @@
-﻿namespace Book.Services
+﻿using Book.Pages;
+
+namespace Book.Services
 {
     public class PageParamsSvc()
     {
@@ -10,6 +12,7 @@
         public int TransactionTypeId { get; set; }
         public int SummaryTypeId { get; set; }
         public List<Transaction> Transactions { get; set; } = [];
+        public string PageTitle { get; set; } = default!;
 
         public void Init()
         {
@@ -21,6 +24,7 @@
             TransactionTypeId = 0;
             SummaryTypeId = 0;
             Transactions = [];
+            PageTitle = string.Empty;
         }
     }
 }
